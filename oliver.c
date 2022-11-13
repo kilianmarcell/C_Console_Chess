7cd1f9bccff53790f2c845cb26cdbe720dc8a854
kilianmarcell <kilian.marcell.andras@gmail.com>
2022-11-13 22:31:55 +0100

    Félkész kész

--- oliver.c
+++ oliver.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
-
-#include <malloc.h>
 
 typedef struct {
-    int *nehezseg;
+    char *nehezseg;
     char *kerdes;
     char *a_valasz;
     char *b_valasz;
@@ -14,34 +11,67 @@ typedef struct {
     char *d_valasz;
     char *megoldas;
     char *kategoria;
-}KERDES;
+} Kerdes;
 
-int main(){
-    KERDES *kerdesek;
-    FILE * open_kerdesek;
-    open_kerdesek = fopen("kerdesek.csv", "r");
-    if(open_kerdesek == NULL)
-        printf("Nem nyilt meg a fajl!\n");
-        
-    char sor[1000];
-    for (int i = 1; EOF != fscanf(open_kerdesek, "%s", sor);++i){
-        KERDES*Uj_kerdesek = (KERDES*)malloc((i+1)*sizeof(KERDES));
-        for(int j = 1; j<i; j++)
-            Uj_kerdesek[j]=kerdesek[j];
-        KERDES ujkerdes;
+void kiir(Kerdes *values, int hossz) {
+    for (int i = 0; i < hossz; i++)
+    printf("%s %s %s %s %s %s %s %s\n", 
+        values[i].nehezseg,
+        values[i].kerdes,
+        values[i].a_valasz,
+        values[i].b_valasz,
+        values[i].c_valasz,
+        values[i].d_valasz,
+        values[i].megoldas,
+        values[i].kategoria);
+    printf("\n");
+}
 
+int main(void)
+{
+  FILE *fp = fopen("kerdesek.csv", "r"); 
+  
+  if (fp == NULL) {
+    printf("Error opening file.\n");
+    return 1;
+  }
+  
+  // array of structs for storing the Student data from the file
+  char buff[1024];
+  int row_count = 0;
+  int field_count = 0;
 
+  Kerdes values[5100];
 
+    int i = 0;
+    while(fgets(buff, 1024, fp)) {
+        field_count = 0;
+        row_count++;
 
-        Uj_kerdesek[i] = ujkerdes;
-       // while(karakter[i] = '\n')
-          //  sor[i] = karakter[i];
-    printf("%s", sor);
-    free(kerdesek);
+        if (row_count == 1) {
+            continue;
+        }
 
+        char *field = strtok(buff, ';');
+
+        while (field) {
+            if (field_count == 0) strcpy(values[i].nehezseg, field);
+            if (field_count == 1) strcpy(values[i].kerdes, field);
+            if (field_count == 2) strcpy(values[i].a_valasz, field);
+            if (field_count == 3) strcpy(values[i].b_valasz, field);
+            if (field_count == 4) strcpy(values[i].c_valasz, field);
+            if (field_count == 5) strcpy(values[i].d_valasz, field);
+            if (field_count == 6) strcpy(values[i].megoldas, field);
+            if (field_count == 7) strcpy(values[i].kategoria, field);
+
+            field = strtok(NULL, ";");
+            field_count++;
+        }
+        i++;
     }
+    fclose(fp);
 
+    kiir(values, row_count);
 
-    fclose(open_kerdesek);
-    return 0;
+  return 0;
 }
--- tempCodeRunnerFile.c
+++
@@ -1,2 +0,0 @@
- "King: \xE2\x99\x94.\n";
-     printf(q);
